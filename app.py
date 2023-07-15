ab5cc9d458743735077dadb560806fdc7912926f
Mohamed Ghoneim <138689320+moghon92@users.noreply.github.com>
2023-07-15 14:58:29 +0100

    accept multiplee docs and cache uploaded docs

--- app.py
+++ app.py
@@ -1,17 +1,18 @@
 import streamlit as st
 import tempfile
 import os
-# from langchain.llms import OpenAI
 from langchain.chat_models import ChatOpenAI
-from langchain.text_splitter import CharacterTextSplitter, RecursiveCharacterTextSplitter
+from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.embeddings import OpenAIEmbeddings
-from langchain.vectorstores import Chroma, FAISS
-from langchain.document_loaders import PyPDFDirectoryLoader
+from langchain.vectorstores import FAISS
+from langchain.document_loaders import PyPDFLoader, Docx2txtLoader, TextLoader
 from langchain.chains import RetrievalQA
 
-
-def read_uploaded_files(uploaded_files):
+@st.cache_resource(show_spinner=False)
+def put_files_in_DB(uploaded_files):
     if uploaded_files is not None:
+        documents = []
+        file_names = []
         temp_dir = tempfile.TemporaryDirectory()
         for uploaded_file in uploaded_files:
             if uploaded_file is not None:
@@ -20,24 +21,28 @@ def read_uploaded_files(uploaded_files):
                 with open(temp_file_path, "wb") as temp_file:
                     temp_file.write(uploaded_file.read())
 
-        r_text_splitter = RecursiveCharacterTextSplitter(
-            separators=["\n\n", "\n", " ", ""],
+                file_names.append(uploaded_file.name)
+
+                # load into Langchain format
+                if temp_file_path.endswith(".pdf"):
+                    loader = PyPDFLoader(temp_file_path)
+                    documents.extend(loader.load())
+                elif temp_file_path.endswith('.docx') or temp_file_path.endswith('.doc'):
+                    loader = Docx2txtLoader(temp_file_path)
+                    documents.extend(loader.load())
+                elif temp_file_path.endswith('.txt'):
+                    loader = TextLoader(temp_file_path)
+                    documents.extend(loader.load())
+
+        # todo: del temp dir
+        # split docs into text
+        text_splitter = RecursiveCharacterTextSplitter(
+            separators=["\n\n", "\n", "(?<=\. )", " ", ""],
             chunk_size=1000,
-            chunk_overlap=32
+            chunk_overlap=100
         )
-        # load into Langchain format
-        loader = PyPDFDirectoryLoader(temp_dir.name)
-        documents = loader.load_and_split(text_splitter=r_text_splitter)
-        return documents
 
-    return None
-
-def generate_response(uploaded_files, openai_api_key, query_text):
-    # Load document if file is uploaded
-    texts = read_uploaded_files(uploaded_files)
-
-    if texts is not None:
-        #documents = [uploaded_file.read().decode()]
+        texts = text_splitter.split_documents(documents)
 
         # Select embeddings
         embeddings = OpenAIEmbeddings(openai_api_key=openai_api_key)
@@ -45,30 +50,49 @@ def generate_response(uploaded_files, openai_api_key, query_text):
         db = FAISS.from_documents(texts, embeddings)
         # Create retriever interface
         retriever = db.as_retriever()
+
+        st.write("Done processing of :", ", ".join([f for f in file_names]))
+
+        return retriever
+
+    return None
+
+def generate_response(retriever, openai_api_key, query_text):
+    if retriever is not None:
         # Create QA chain
         qa = RetrievalQA.from_chain_type(llm=ChatOpenAI(temperature=0.0, openai_api_key=openai_api_key), chain_type='stuff', retriever=retriever)
         return qa.run(query_text)
 
 # Page title
-st.set_page_config(page_title="Chat with Doc", page_icon=":books:")
-st.title('Chat with your Document')
+st.set_page_config(page_title="Chat with Docs", page_icon=":books:")
+st.title('Chat with your Documents :book:')
 
+# read the user's openAI key
+openai_api_key = st.text_input('OpenAI API Key',placeholder='sk-', type='password')
 # File upload
-#uploaded_file = st.file_uploader('Upload a txt file', type='txt')
-uploaded_files = st.file_uploader("Upload your files here", accept_multiple_files=True)
-# Query text
-query_text = st.text_input('Enter your question:', placeholder = 'Please provide a short summary.', disabled=not uploaded_files)
+uploaded_files = st.file_uploader("Upload your files here"
+                                  , type=['pdf', 'docx', 'doc', 'txt']
+                                  , accept_multiple_files=True
+                                  , disabled=not openai_api_key
+                                )
 
-# Form input and query
-result = []
-with st.form('myform', clear_on_submit=False):
-    openai_api_key = st.text_input('OpenAI API Key', type='password', disabled=not (uploaded_files and query_text))
-    submitted = st.form_submit_button('Submit', disabled=not(uploaded_files and query_text))
-    if submitted and openai_api_key.startswith('sk-'):
-        with st.spinner('Calculating...'):
-            response = generate_response(uploaded_files, openai_api_key, query_text)
-            result.append(response)
-            # del openai_api_key
+# Load document if file is uploaded
+if len(uploaded_files) > 0:
+    with st.spinner('Processing...'):
+        retriever = put_files_in_DB(uploaded_files)
 
-if len(result):
-    st.info(response)
+    # Form input and query
+    result = []
+    with st.form('myform', clear_on_submit=False):
+        # Query text
+        query_text = st.text_input('type your question:', placeholder='Please provide a short summary.')
+        # submit key
+        submitted = st.form_submit_button('Submit')
+        if submitted and openai_api_key.startswith('sk-'):
+            with st.spinner('Calculating...'):
+                response = generate_response(retriever, openai_api_key, query_text)
+                result.append(response)
+                del openai_api_key
+
+    if len(result):
+        st.info(response)
